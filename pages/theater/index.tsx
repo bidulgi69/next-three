4a6a88ed83dcb898f9b85996d1db819456b97743
bidulgi69 <rhyme884@gmail.com>
2022-02-27 01:27:17 +0900

    Add guidelines, lazy load (suspense)

--- pages/theater/index.tsx
+++ pages/theater/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
-import { TrackballControls } from "@react-three/drei";
+import { PointerLockControls } from "@react-three/drei";
 import { Lights, Video } from "../../meshes";
+import {Loading} from "../../components";
 
 export default function Theater() {
     const leftCurtain: React.MutableRefObject<HTMLDivElement | null> = React.useRef(null)
@@ -15,6 +16,7 @@ export default function Theater() {
 
     return (
         <>
+
             <div className="container">
                 <div className="curtain__wrapper"
                      onClick={() => openCurtain()}>
@@ -23,42 +25,46 @@ export default function Theater() {
                         ref={leftCurtain}
                     >
                     </div>
-                    <div className={"theater"}>
-                        <Canvas>
-                            <TrackballControls
-                                position={[0, 0, 0]}
-                                noZoom
-                            />
-                            <Lights />
-                            <Video
-                                position={new THREE.Vector3(0, 0, 24)}
-                                rotation={new THREE.Euler(0, 0, 0)}
-                                videoId={"QmejC5HSHRw"}
-                            />
-                            <Video
-                                position={new THREE.Vector3(24, 0, 0)}
-                                rotation={new THREE.Euler(0, 90 * (Math.PI / 180), 0)}
-                                videoId={"ormQQG2UhtQ"}
-                            />
-                            <Video
-                                position={new THREE.Vector3(-24, 0, 0)}
-                                rotation={new THREE.Euler(0, 270 * (Math.PI / 180), 0)}
-                                videoId={"xBa3YUgQeL4"}
-                            />
-                            <Video
-                                position={new THREE.Vector3(0, 0, -24)}
-                                rotation={new THREE.Euler(0, 180 * (Math.PI / 180), 0)}
-                                videoId={"9QzUzQBuPWw"}
-                                autoplay
-                            />
-                        </Canvas>
-                    </div>
+                    <Suspense fallback={<Loading message={"Opening Theater..."} />}>
+                        <div className={"theater"}>
+                            <Canvas>
+                                <PointerLockControls
+                                    position={[0, 0, 0]}
+                                />
+                                <Lights />
+                                <Video
+                                    position={new THREE.Vector3(0, 0, 24)}
+                                    rotation={new THREE.Euler(0, 0, 0)}
+                                    videoId={"QmejC5HSHRw"}
+                                />
+                                <Video
+                                    position={new THREE.Vector3(24, 0, 0)}
+                                    rotation={new THREE.Euler(0, 90 * (Math.PI / 180), 0)}
+                                    videoId={"ormQQG2UhtQ"}
+                                />
+                                <Video
+                                    position={new THREE.Vector3(-24, 0, 0)}
+                                    rotation={new THREE.Euler(0, 270 * (Math.PI / 180), 0)}
+                                    videoId={"xBa3YUgQeL4"}
+                                />
+                                <Video
+                                    position={new THREE.Vector3(0, 0, -24)}
+                                    rotation={new THREE.Euler(0, 180 * (Math.PI / 180), 0)}
+                                    videoId={"9QzUzQBuPWw"}
+                                    autoplay
+                                />
+                            </Canvas>
+                        </div>
+                    </Suspense>
                     <div
                         className={`curtain__panel right`}
                         ref={rightCurtain}
                     >
                     </div>
                 </div>
+                <p>
+                    Press Escape(esc) to unlock and click empty space to lock controls.
+                </p>
             </div>
             <style jsx>{`
                 .container {
@@ -71,7 +77,6 @@ export default function Theater() {
                 }
                 .curtain__wrapper {
                     width: 100%;
-                    height: 100%;
                     display: flex;
                     justify-content: center;
                 }
@@ -101,6 +106,24 @@ export default function Theater() {
                     z-index: 1; 
                     width: calc(100% - 50px);
                     height: 719px;
+                }
+                p {
+                    color: #fff;
+                    font-weight: bold;
+                    font-size: 30pt;
+                    animation: fade-in 5s;
+                    animation-fill-mode: forwards;
+                }
+                p:hover {
+                    cursor: pointer;
+                }
+                @keyframes fade-in {
+                    0% {
+                        opacity: 0;
+                    }
+                    100% {
+                        opacity: 1;
+                    }
                 }
             `}
             </style>
